1161c6d3168ac71b7df904e2537f5f5388fffb90
Ramya Prabhu <ramya_prabhu@glic.com>
2018-10-04 23:52:38 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 console.log("Up and running!");
 
-var cardOne="queen";
-var cardTwo="queen";
-var cardThree="king";
-var cardFour="king";
 
-console.log("User flipped " + cardOne);
+var cards =["queen","queen","king","king"];
+var cardsInPlay =[];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User Flipped"+cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User Flipped "+cardTwo);
+
+if (cardsInPlay.length === 2) {
+
+	console.log("Two cards flipped");
+
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match");
+	}else{
+		alert("Sorry try again");
+	}
+}
