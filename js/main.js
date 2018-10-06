57d43185d0a6c0c840c566994ecbbc09a9ebeff8
sydgr6p <ramya_prabhu@glic.com>
2018-10-05 22:07:06 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -4,23 +4,36 @@ console.log("Up and running!");
 var cards =["queen","queen","king","king"];
 var cardsInPlay =[];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User Flipped"+cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User Flipped "+cardTwo);
-
-if (cardsInPlay.length === 2) {
-
-	console.log("Two cards flipped");
-
+function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match");
-	}else{
-		alert("Sorry try again");
+			alert("You found a match");
+		}else{
+			alert("Sorry try again");
+		}
+}
+
+function flipCard(cardId){	
+/*
+	var cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User Flipped"+cardOne);
+
+	var cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User Flipped "+cardTwo);
+*/
+
+	console.log("User Flipped"+ cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+
+		console.log("Two cards flipped");
+		checkForMatch();
+		
 	}
 }
+
+flipCard(0);
+flipCard(1);
+
+
